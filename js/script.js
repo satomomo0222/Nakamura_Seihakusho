7fe1f1a16b329249399eb382dab13e464e1d3ef7
satomomo0222 <satomomo0222@gmail.com>
2020-05-07 00:20:41 +0900

    add intror

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 $(function(){
-  $('.slide-list').slick({
+  $('.slide').slick({
+    centerMode: true,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 5000,
     pauseOnFocus: false,
     pauseOnHover: false,
     pauseOnDotsHover: false,
-    dots: true,
+    swipe: false,
+    swipeToSlide: false,
+    dots: false,
     infinite: true,
-    speed: 2000,
+    speed: 3000,
     fade: true,
     cssEase: 'linear',
     arrows: false
+  }).on({
+    beforeChange: function(event, slick, currentSlide, nextSlide) {
+      $(".slick-slide", this).eq(currentSlide).addClass("preve-slide");
+      $(".slick-slide", this).eq(nextSlide).addClass("slide-animation");
+    },
+    afterChange: function() {
+      $(".preve-slide", this).removeClass("preve-slide slide-animation");
+    }
   });
+  $('.slide').find(".slick-slide").eq(0).addClass("slide-animation");
 
   $('#buy').hover(function(){
     $("#bounce").addClass("bounce");
